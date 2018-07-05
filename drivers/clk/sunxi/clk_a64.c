34f11b8681c00f9b5a45b30e4eb4f527a75a08f6
Jagan Teki <jagan@amarulasolutions.com>
2018-07-05 22:50:37 +0530

    clk: sunxi: a64: Fix checkpatch.pl warnings
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/clk/sunxi/clk_a64.c
+++ drivers/clk/sunxi/clk_a64.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
 /*
  * (C) Copyright 2018 Arm Ltd.
  *
@@ -18,7 +19,7 @@ struct a64_clk_priv {
 
 static ulong a64_clk_get_rate(struct clk *clk)
 {
-	debug("%s(#%ld)\n", __FUNCTION__, clk->id);
+	debug("%s(#%ld)\n", __func__, clk->id);
 
 	debug("  unhandled\n");
 	return -EINVAL;
@@ -26,7 +27,7 @@ static ulong a64_clk_get_rate(struct clk *clk)
 
 static ulong a64_clk_set_rate(struct clk *clk, ulong rate)
 {
-	debug("%s(#%ld, rate: %lu)\n", __FUNCTION__, clk->id, rate);
+	debug("%s(#%ld, rate: %lu)\n", __func__, clk->id, rate);
 
 	debug("  unhandled\n");
 	return -EINVAL;
@@ -34,7 +35,7 @@ static ulong a64_clk_set_rate(struct clk *clk, ulong rate)
 
 static int a64_clk_enable(struct clk *clk)
 {
-	debug("%s(#%ld)\n", __FUNCTION__, clk->id);
+	debug("%s(#%ld)\n", __func__, clk->id);
 
 	debug("  unhandled\n");
 	return -EINVAL;
@@ -61,16 +62,16 @@ static int a64_clk_ofdata_to_platdata(struct udevice *dev)
 }
 
 static const struct udevice_id a64_clk_ids[] = {
-        { .compatible = "allwinner,sun50i-a64-ccu" },
-        { }
+	{ .compatible = "allwinner,sun50i-a64-ccu" },
+	{ }
 };
 
 U_BOOT_DRIVER(clk_sun50i_a64) = {
-        .name           = "sun50i-a64-ccu",
-        .id             = UCLASS_CLK,
-        .of_match       = a64_clk_ids,
-        .priv_auto_alloc_size = sizeof(struct a64_clk_priv),
-        .ofdata_to_platdata = a64_clk_ofdata_to_platdata,
-        .ops            = &a64_clk_ops,
-        .probe          = a64_clk_probe,
+	.name		= "sun50i-a64-ccu",
+	.id		= UCLASS_CLK,
+	.of_match	= a64_clk_ids,
+	.priv_auto_alloc_size	= sizeof(struct a64_clk_priv),
+	.ofdata_to_platdata	= a64_clk_ofdata_to_platdata,
+	.ops		= &a64_clk_ops,
+	.probe		= a64_clk_probe,
 };
